9c46bb6b298ca6b6d3b8241e3b03de54a84fbd6f
João Vitor <vitorjoao39207@gmail.com>
2025-03-26 14:10:07 -0300

    create login and register form

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,12 @@
 import Input from "@/components/input";
+import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <div className="w-[400px] h-[500px] border border-gray-200 rounded  p-5 flex flex-col gap-5">
-
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold">Login</h1>
-          <span className="text-sm text-gray-400 font-sans">Digite seu email abaixo para entrar na sua conta.</span>
-        </div>
-
-        <div className="flex flex-col gap-1 text-base">
-          <span>Email</span>
-          <Input /> 
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <span>Senha</span>
-          <Input /> 
-        </div>
-
-        <div>
-          <button>Entrar</button>
-        </div>
-
+    <div className="flex justify-center items-center min-h-svh">
+      <div className="w-full max-w-sm">
+        <LoginForm />
       </div>
     </div>
   );
